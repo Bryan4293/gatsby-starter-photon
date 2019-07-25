41166564d6ff205959247290530f772cb4dad9a2
Bryan <Jbrewton88@gmail.com>
2019-07-25 12:23:34 -0500

    Commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,9 @@ class Footer extends React.Component {
         return (
             <section id="footer">
                 <ul className="icons">
-                    {/* <li><a href="#" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
+                    <li><a href="#" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
                     <li><a href="#" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
-                    <li><a href="#" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li> */}
+                    <li><a href="#" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
                     <li><a href="https://github.com/Bryan4293" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
                     <li><a href="https://www.linkedin.com/in/bryan-brewton-837423186/" className="icon alt fa-linkedin"><span className="label">LinkedIn</span></a></li>
                 </ul>
