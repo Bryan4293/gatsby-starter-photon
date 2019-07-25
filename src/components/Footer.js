65ed2bccf9d0f484607fbdb8a285992cfab2d07d
Bryan <Jbrewton88@gmail.com>
2019-07-25 11:31:09 -0500

    edit index

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,11 @@ class Footer extends React.Component {
         return (
             <section id="footer">
                 <ul className="icons">
-                    <li><a href="#" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
+                    {/* <li><a href="#" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
                     <li><a href="#" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
-                    <li><a href="#" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
-                    <li><a href="#" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
-                    <li><a href="#" className="icon alt fa-envelope"><span className="label">Email</span></a></li>
+                    <li><a href="#" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li> */}
+                    <li><a href="https://github.com/Bryan4293" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
+                    <li><a href="https://www.linkedin.com/in/bryan-brewton-837423186/" className="icon alt fa-linkedin"><span className="label">LinkedIn</span></a></li>
                 </ul>
                 <ul className="copyright">
                     <li>&copy; Untitled</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
